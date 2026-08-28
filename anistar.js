4e9384f5b208ebc0ea1c41b9a49780e9dd341fdf
ilwsm <ua.andrey@rocketmail.com>
2026-08-28 18:23:54 +0300

    Restrict AniStar search to video categories

--- anistar.js
+++ anistar.js
@@ -9,6 +9,17 @@
     var DEBUG_KEY = 'anistar_debug_log';
     var DEBUG_ENABLED_KEY = 'anistar_logging';
     var DEBUG_LIMIT = 100;
+    var VIDEO_CATS = [
+        35, 36, 39,
+        3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25, 26, 27,
+        29, 30, 31, 32, 33, 34, 74, 79, 81, 111, 112, 114, 115, 116, 117, 118, 119, 120,
+        121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133,
+        76, 83, 84, 85, 86, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101,
+        102, 103, 104, 105, 106, 107, 108, 109, 110,
+        113, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148,
+        149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164,
+        165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175
+    ];
 
     function debugEnabled() {
         var value = Lampa.Storage.get(DEBUG_ENABLED_KEY, true);
@@ -155,7 +166,10 @@
     }
 
     function searchRequest(query, callback) {
-        var body = 'do=search&subaction=search&search_start=1&full_search=1&result_from=1&titleonly=3&searchdate=0&beforeafter=after&sortby=date&resorder=asc&showposts=0&catlist%5B%5D=39&catlist%5B%5D=35&story=' + encode1251(query);
+        var catlist = VIDEO_CATS.map(function (category) {
+            return 'catlist%5B%5D=' + category;
+        }).join('&');
+        var body = 'do=search&subaction=search&search_start=1&full_search=1&result_from=1&resorder=asc&titleonly=3&searchdate=0&beforeafter=after&sortby=date&showposts=0&' + catlist + '&story=' + encode1251(query);
         var searchUrl = BASE + '/index.php?action_skin_change=1&skin_name=smartphone';
         var req = new Lampa.Reguest();
         req.timeout(20000);
